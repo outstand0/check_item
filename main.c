6cd3669a4b6282eb65520b27715d58ca47aea3d1
youngsu <youngsu@sk.com>
2024-08-16 11:02:30 +0900

    test_temp commit

--- main.c
+++ main.c
@@ -7,7 +7,9 @@ void main()
 {
 
 
-    test_temp[0] = 2; // want check point
+    test_temp[0] = 1; // want check point
     test_temp[1] = 0;
     test_temp[2] = 1;
+    test_temp[3] = 3;
+    test_temp[4] = 5;
 }
